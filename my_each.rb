8bb56248b60188d7640e00055efdc7eab056f969
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 12:28:25 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each(collection)
   if block_given?
  i = 0 
- while i < collection.length
- yield(array[i])
- puts i
- i = i + 1
- end
+  while i < collection.length
+    yield(array[i])
+    i = i + 1
+  end
  else
    puts "Hey! No block was given."
  end
